7d99e7f78285650e9437d200c3be5dee50605b23
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-25 15:58:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,3 @@
-require 'pry'
 # Your code goes here!
 class Anagram
   attr_reader = :word 
@@ -9,10 +8,5 @@ class Anagram
   
   def match(array)
     matched = array.find_all {|a_word| a_word.split(//).sort == @word.split(//).sort}
-    #binding.pry 
-    
   end 
 end 
-
-#detector = Anagram.new('ba')
-#ba = detector.match(['ab', 'abc', 'bac'])
