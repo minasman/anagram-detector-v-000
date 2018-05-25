e545d3242d9c526e13aff4de00347c40e0c4ba39
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-25 15:38:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+def Anagram
+  attr_reader = :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    matched = array.map {|a_word| a_word.sort == @word.sort} 
+  end 
+end 
