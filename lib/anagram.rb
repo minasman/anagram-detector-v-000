cb366d044122f1270353d181626756f228dc0090
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-25 15:52:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,11 +8,11 @@ class Anagram
   end
   
   def match(array)
-    matched = array.find_all {|a_word| a_word.split.sort == @word.split.sort}
-    binding.pry 
+    matched = array.find_all {|a_word| a_word.split(//).sort == @word.split(//).sort}
+    #binding.pry 
     
   end 
 end 
 
-detector = Anagram.new('ba')
-ba = detector.match(['ab', 'abc', 'bac'])
+#detector = Anagram.new('ba')
+#ba = detector.match(['ab', 'abc', 'bac'])
